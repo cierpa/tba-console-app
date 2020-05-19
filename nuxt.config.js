44b6a38d47fe15487576c6b847a8d10b11548fc8
vadim petrov <isac@vmp.local>
2020-05-19 18:37:00 +0900

    ログイン画面のデザインはほぼ出来上がり？

--- nuxt.config.js
+++ nuxt.config.js
@@ -69,12 +69,13 @@ export default {
     customVariables: ['~/assets/variables.scss'],
     theme: {
       themes: {
-        dark: {
+        light: {
           primary: colors.blue.darken2,
           accent: colors.grey.darken3,
           secondary: colors.amber.darken3,
           info: colors.teal.lighten1,
-          warning: colors.amber.base,
+          // warning: colors.amber.base,
+          warning: '#FF6D4C',
           error: colors.deepOrange.accent4,
           success: colors.green.accent3
         }
